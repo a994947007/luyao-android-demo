b8f23691828fb56a87c523a861d9dea33a5f0944
a994947007 <994947007@qq.com>
2022-02-15 02:00:48 +0800

    webView bug修复

--- app/src/main/java/com/hc/android_demo/commands/OpenActivityCommand.java
+++ app/src/main/java/com/hc/android_demo/commands/OpenActivityCommand.java
@@ -1,11 +1,10 @@
 package com.hc.android_demo.commands;
 
-import android.content.ComponentName;
-import android.content.Intent;
-
 import com.google.auto.service.AutoService;
 import com.hc.base.AppEnvironment;
+import com.hc.util.ActivityUtils;
 import com.hc.util.ThreadUtils;
+import com.jny.common.fragment.FragmentConstants;
 import com.jny.common.webview.Command;
 import com.jny.common.webview.WebViewCallback;
 
@@ -16,10 +15,7 @@ public class OpenActivityCommand implements Command {
     @Override
     public void execute(Map<String, String> params, WebViewCallback callback) {
         ThreadUtils.runInMainThread(() -> {
-            Intent intent = new Intent();
-            intent.setComponent(new ComponentName(AppEnvironment.getAppContext(),"com.hc.android_demo.activity.test.view.GridImageLayoutActivity"));
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            AppEnvironment.getAppContext().startActivity(intent);
+            ActivityUtils.startContentActivity(AppEnvironment.getAppContext(), FragmentConstants.GRID_IMAGE_LAYOUT_TEST_FRAGMENT_ID);
         });
     }
 
--- util/src/main/java/com/hc/util/ActivityUtils.java
+++ util/src/main/java/com/hc/util/ActivityUtils.java
@@ -1,5 +1,6 @@
 package com.hc.util;
 
+import android.content.Context;
 import android.content.Intent;
 
 import com.hc.base.activity.Constants;
@@ -11,5 +12,12 @@ public final class ActivityUtils {
         Intent intent = new Intent(luActivity, ContentActivity.class);
         intent.putExtra(com.hc.base.activity.Constants.CONTENT_FRAGMENT_KEY, id);
         luActivity.startActivity(intent);
+    }
+
+    public static void startContentActivity(Context context, String id) {
+        Intent intent = new Intent(context, ContentActivity.class);
+        intent.putExtra(com.hc.base.activity.Constants.CONTENT_FRAGMENT_KEY, id);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
     }
 }
