64dbce4f57e2212aeee974a66ffd8eff134840fe
a994947007 <994947007@qq.com>
2022-06-15 04:05:40 +0800

    侧滑动画跟ViewPage切换动画同时进行

--- app/src/main/java/com/hc/android_demo/fragment/content/SwipeGuideFragment.java
+++ app/src/main/java/com/hc/android_demo/fragment/content/SwipeGuideFragment.java
@@ -1,6 +1,10 @@
 package com.hc.android_demo.fragment.content;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +13,12 @@ import android.view.animation.TranslateAnimation;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.auto.service.AutoService;
 import com.hc.android_demo.R;
@@ -20,9 +30,9 @@ import com.jny.common.fragment.FragmentConstants;
 @AutoService({ActivityStarter.class})
 public class SwipeGuideFragment extends Fragment implements ActivityStarter {
 
+    private ViewPager mViewPage;
     private View mShadowLayout;
-
-    private TranslateAnimation translateAnimation;
+    private ValueAnimator translateAnimation;
 
     @Nullable
     @Override
@@ -30,14 +40,49 @@ public class SwipeGuideFragment extends Fragment implements ActivityStarter {
         return inflater.inflate(R.layout.fragment_swipe_guide_layout, container, false);
     }
 
+    private int mAnimateX = 0;
+    private int mDragX = 0;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mViewPage = view.findViewById(R.id.viewPager);
+        mViewPage.setAdapter(new MyPageAdapter(getFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT));
+
+        mViewPage.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                mDragX = -positionOffsetPixels;
+                int current = mAnimateX + mDragX;
+                mShadowLayout.setTranslationX(current);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state == ViewPager.SCROLL_STATE_DRAGGING) { // start scroll
+                    translateAnimation.start();
+                }
+            }
+        });
+        // 动画相关
         mShadowLayout = view.findViewById(R.id.swipe_shadow);
         mShadowLayout.setBackgroundDrawable(new SwipeShadow());
-        translateAnimation = new TranslateAnimation(0, -ViewUtils.dp2px(59f), 0, 0);
-        translateAnimation.setDuration(2000);
-        mShadowLayout.setAnimation(translateAnimation);
+        translateAnimation = ValueAnimator.ofFloat(0, 1);
+        translateAnimation.setDuration(500);
+        translateAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float ratio = (float) animation.getAnimatedValue();
+                mAnimateX = -(int) (ratio * mShadowLayout.getMeasuredWidth());
+                int currentX = (int) (mDragX + mAnimateX);
+                mShadowLayout.setTranslationX(currentX);
+            }
+        });
     }
 
     @Override
@@ -49,5 +94,23 @@ public class SwipeGuideFragment extends Fragment implements ActivityStarter {
     @Override
     public Fragment getContentFragment() {
         return new SwipeGuideFragment();
+    }
+
+    private static class MyPageAdapter extends FragmentPagerAdapter {
+
+        public MyPageAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return new SwipeGuideItemFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
---
+++ app/src/main/java/com/hc/android_demo/fragment/content/SwipeGuideItemFragment.java
@@ -0,0 +1,26 @@
+package com.hc.android_demo.fragment.content;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.hc.android_demo.R;
+
+public class SwipeGuideItemFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_swipe_guide_item_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
