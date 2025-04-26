d85b181758ae8bd0cbd8a6f763c6d4281497a290
luyao <994947007@qq.com>
2025-04-27 04:11:14 +0800

    observable复用

--- base/src/main/java/com/hc/base/framework/BeanObservableExt.kt
+++ base/src/main/java/com/hc/base/framework/BeanObservableExt.kt
@@ -3,6 +3,10 @@ package com.hc.base.framework
 import com.android.demo.rxandroid.observable.Observable
 
 fun <T: Syncable> T.observable(): Observable<T> {
+    val rxObservableCache = RxObservableManager.get(this)
+    if (rxObservableCache != null) {
+        return rxObservableCache.observable() as Observable<T>
+    }
     val rxObservable = DefaultRxObservable<T>()
     RxObservableManager.addRxObservable(this, rxObservable)
     return rxObservable.observable()
--- base/src/main/java/com/hc/base/framework/RxObservableManager.kt
+++ base/src/main/java/com/hc/base/framework/RxObservableManager.kt
@@ -1,6 +1,7 @@
 package com.hc.base.framework
 
 object RxObservableManager {
+    // 这会使得user对象越多，这里的RxObservable也会增加，而且没有释放
     private val observableMap = mutableMapOf<String, RxObservable<out Syncable>>()
 
     fun addRxObservable(syncable: Syncable, rxObservable: RxObservable<out Syncable>) {
